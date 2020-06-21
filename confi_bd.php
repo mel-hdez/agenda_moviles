32bf2bdd74dc67f5b018ad8a4983aa839e4169be
mel <melhernandez236@melweb.com>
2020-06-21 17:42:30 -0500

    login y confibd

--- confi_bd.php
+++ confi_bd.php
@@ -1,13 +1,13 @@
-<? php
-  $ db_host = "us-cdbr-east-06.cleardb.net" ;
-  $ db_user = "bc299bd08b7625" ;
-  $ db_pass = "b0f90a1e" ;
-  $ db_name = "heroku_cfd3a4efcdf1efd" ;
-
-  $ conn = new mysqli ( $ db_host , $ db_user , $ db_pass , $ db_name );
-  if ( $ conn -> connect_error ) {
+<?php
+  $db_host ="us-cdbr-east-06.cleardb.net";
+  $db_user = "bc299bd08b7625";
+  $db_pass = "b0f90a1e";
+  $db_name = "heroku_cfd3a4efcdf1efd";
+  
+  $conn = new mysqli ( $db_host , $db_user , $db_pass , $db_name );
+  if ( $conn -> connect_error ) {
     echo  "Fallo";
-    morir ( "Error de conexión:" . $ conn -> connect_error );
+    morir ( "Error de conexión:" . $conn -> connect_error );
   }
 ?>
 
