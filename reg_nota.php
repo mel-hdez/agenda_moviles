b41f177d5f606396625e8522c5660e0a831d5a8e
mel <melhernandez236@melweb.com>
2020-06-22 07:23:56 -0500

    prueba notas2

--- reg_nota.php
+++ reg_nota.php
@@ -2,11 +2,11 @@
 
 include 'confi_bd.php';
 
-    $notatitulo = filter_input (INPUT_POST,"titulo");
-    $texnota = filter_input (INPUT_POST,"texto");
+    $notatitulo = filter_input (INPUT_POST,"nom_notas");
+    $texnota = filter_input (INPUT_POST,"desc_notas");
     $fecha = filter_input (INPUT_POST,"fecha");
         
-         $usuarioQuery = "INSERT INTO nota (titulo, texto, fecha)
+         $usuarioQuery = "INSERT INTO notas (nom_notas, desc_notas, fecha)
                               VALUES('$notatitulo','$texnota', '$fecha')";
             $userResult = mysqli_query($conn, $usuarioQuery);
             if($userResult){
