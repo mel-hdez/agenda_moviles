08655d8f9cf1243f247d51ca83d8f64a5bd1d531
mel <melhernandez236@melweb.com>
2020-06-22 08:10:10 -0500

    prubea nota

--- reg_nota.php
+++ reg_nota.php
@@ -4,7 +4,7 @@ include 'confi_bd.php';
 
     $id = filter_input (INPUT_POST,"id");
     $notatitulo = filter_input (INPUT_POST,"titulo");
-    $texnota = filter_input (INPUT_POST,"desc_notas");
+    $texnota = filter_input (INPUT_POST,"texto");
     $fecha = filter_input (INPUT_POST,"fecha");
         
          $usuarioQuery = "INSERT INTO notas (nom_notas, desc_notas, fecha, id_usuario)
