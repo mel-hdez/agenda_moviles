7c46dd12a9c69b81f1e779556b50c5c7045a83f9
Luis Fernando Barajas Cortes <f1nandocor@gmail.com>
2020-06-22 10:10:48 -0500

    notas5

--- notas.php
+++ notas.php
@@ -5,7 +5,7 @@
   $id = $_REQUEST['id'];
   $resultadoArray = array();
     
-    $notasQuery = "SELECT * FROM notas WHERE id_usuario = '$id'";
+    $notasQuery = "SELECT * FROM notas WHERE id_usuario = '".$id."'";
     $notasResult = mysqli_query($conn, $notasQuery);
     foreach($notasResult as $row){
       $resultadoArray[] = $row;
