55e983489a85ed528ddd4e24d33c19f82196fb87
mel <melhernandez236@melweb.com>
2020-06-21 19:21:20 -0500

    reg_usuario1

--- reg_usuario.php
+++ reg_usuario.php
@@ -2,17 +2,17 @@
 
 include 'confi_bd.php';
 
-    $tipo = filter_input (INPUT_POST,"nombre");
+    $nom = filter_input (INPUT_POST,"nombre");
     $email = filter_input (INPUT_POST,"email");
     $contras = filter_inputT (INPUT_POST,"contrasena");
 
         $revisarQuery = "SELECT * FROM usuario WHERE Correo = '$correo'";
         $revisarResultado = mysqli_query($conn, $revisarQuery);
             $usuarioQuery = "INSERT INTO usuario(nombre, email, contrasena)
-                              VALUES('$nombre','$email', '$contras')";
+                              VALUES('$nom','$email', '$contras')";
             $userResult = mysqli_query($conn, $usuarioQuery);
             if($userResult){
               echo "1";
-            
+            }
 ?>
 
